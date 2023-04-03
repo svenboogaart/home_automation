0b3afcda9ccf259211222b846da0ab298d97d6dc
Sven van den Boogaart <sven.ommel@hotmail.com>
2023-04-03 23:46:38 +0200

    Add user interface to turn lights on and off.

--- web_interface/front/urls.py
+++ web_interface/front/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("light/<str:light_id>/on", views.light_on),
+    path("light/<str:light_id>/off", views.light_of),
     path("lights-off", views.lights_off, name="index"),
     path("lights-on", views.lights_on, name="index"),
 ]
