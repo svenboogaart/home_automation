80cb0d60322dd68c5e84899bc3ede00d9e1518bf
Sven van den Boogaart <sven.ommel@hotmail.com>
2024-12-18 22:47:31 +0100

    remove twilio dependency
    Signed-off-by: Sven van den Boogaart <sven.ommel@hotmail.com>

--- models/managers/sms_manager.py
+++ models/managers/sms_manager.py
@@ -1,4 +1,4 @@
-from twilio.rest import Client
+# from twilio.rest import Client
 
 from settings.settings import Settings
 
@@ -11,12 +11,13 @@ class SmsManager:
 
     def send_sms(self, content):
         if self.__settings.twilio_account_ssd:
-            client = Client(self.__settings.twilio_account_ssd, self.__settings.twilio_auth_token)
-
-            message = client.messages.create(
-                from_="svenhome",
-                body=content,
-                to=self.__settings.sms_receiver_number
-            )
-            self.sms_send_after_alarm_activated = True
-            print(f"Sms has been send: {message.sid}")
+            # client = Client(self.__settings.twilio_account_ssd, self.__settings.twilio_auth_token)
+            #
+            # message = client.messages.create(
+            #     from_="svenhome",
+            #     body=content,
+            #     to=self.__settings.sms_receiver_number
+            # )
+            # self.sms_send_after_alarm_activated = True
+            # print(f"Sms has been send: {message.sid}")
+            print("TODO implement sending sms")
